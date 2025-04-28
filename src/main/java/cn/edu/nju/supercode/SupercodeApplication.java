f0674afc46e920292e017cc455232d8cc926297a
jy <231880439@smail.nju.edu.cn>
2025-04-28 10:17:34 +0800

    initialize

---
+++ src/main/java/cn/edu/nju/supercode/SupercodeApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.supercode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercodeApplication.class, args);
+	}
+
+}
